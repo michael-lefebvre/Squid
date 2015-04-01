405196ad51f5ac576d3e15b182bc849a6905d271
Michael Lefebvre <michael@scenedata.com>
2015-04-01 19:34:01 +0200

    manage credentials

--- src/js/squid.js
+++ src/js/squid.js
@@ -53,6 +53,13 @@ SquidCore.prototype.init = function()
 
   Win.on('blur', _.bind( this.hide, this ) )
 
+  try {
+    this.getCredentials()
+  }
+  catch( e ) {
+    console.warn(  e.message )
+  }
+
   var credentials  = 
       {
           username: 'michael@scenedata.com'
@@ -67,12 +74,12 @@ var myXHR = Xhr(
     {
       Authorization: 'Basic ' + encode
     }
-  , success : function()
+  , success : function( response )
     {
       console.log('success')
-      console.log( this )
+      console.log( response )
     }
-  , error : function()
+  , error : function( response, status )
     {
       console.warn('error')
       console.log( this )
@@ -82,13 +89,12 @@ var myXHR = Xhr(
   return this
 }
 
-// APP DISPLAY
+// WINDOW DISPLAY
 //-------------------------------
 
 // Display App
 SquidCore.prototype.show = function( event )
 {
-console.log('call show')
   if( this._isVisible )
   {
     this.hide()
@@ -109,7 +115,6 @@ SquidCore.prototype.hide = function()
   if( !this._isVisible )
     return
 
-  console.log('hideWindow')
   Win.hide()
   Win.blur()
 
@@ -119,118 +124,6 @@ SquidCore.prototype.hide = function()
 
 // EVENTS
 //-------------------------------
-
-// // Backbone's events shortcuts
-
-// // Bind an event to a `callback` function. Passing `"all"` will bind
-// // the callback to all events fired.
-// // 
-// //      @param   {string}   event's name
-// //      @param   {function} the callback
-// //      @param   {function} scope context
-// //      @return  {void}
-// //
-// SquidCore.prototype.on = function( name, callback, context )
-// {
-//   this._event.on( name, callback, context )
-// }
-
-// // Bind an event to only be triggered a single time. After the first time
-// // the callback is invoked, it will be removed.
-// // 
-// //      @param   {string}   event's name
-// //      @param   {function} the callback
-// //      @param   {function} scope context
-// //      @return  {void}
-// //
-// SquidCore.prototype.once = function( name, callback, context )
-// {
-//   this._event.once( name, callback, context )
-// }
-
-
-// // Remove one or many callbacks. If `context` is null, removes all
-// // callbacks with that function. If `callback` is null, removes all
-// // callbacks for the event. If `name` is null, removes all bound
-// // callbacks for all events.
-// //
-// //      @return  {void}
-// //
-
-// SquidCore.prototype.off = function( name, callback, context )
-// {
-//   this._event.off( name, callback, context )
-// }
-
-// // Trigger one or many events, firing all bound callbacks. Callbacks are
-// // passed the same arguments as `trigger` is, apart from the event name
-// // (unless you're listening on `"all"`, which will cause your callback to
-// // receive the true name of the event as the first argument).
-// // 
-// //      @return  {void}
-// //
-// // TODO: `args1... oh my god ! need to find a better solution
-// SquidCore.prototype.trigger = function( args1, args2, args3, args4, args5, args6, args7 )
-// {
-// /*
-// var args = [].slice.call( arguments, 1 )
-// console.log( arguments )
-// this._event.trigger.apply( this, arguments )
-// */
-
-//   this._event.trigger( args1, args2, args3, args4, args5, args6, args7 )
-// }
-
-
-// COOKIES
-//-------------------------------
-
-
-// get Cookie
-//
-//      return  {mixed}
-//
-SquidCore.prototype.getCookie = function( cookieName )
-{
-  console.groupCollapsed( 'get cookie: ' + cookieName )
-
-  if( !document.cookie )
-  {
-    console.warn('no cookie for this domain')
-    console.groupEnd()
-    return false
-  }
-
-  // retrive data from cookies
-  var data = decodeURIComponent( 
-    document.cookie.replace( 
-      new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent( cookieName ).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1"
-    )
-  ) || null
-  
-  console.log( document.cookie )
-  console.log( data )
-  
-  if( data === 'null' )
-  {
-    console.warn('cookie "' + cookieName + '" found but data are null')
-    console.groupEnd()
-
-    return false
-  }
-
-  console.groupEnd()
-  return data
-}
-
-// set Cookie
-//
-//      return  {void}
-//
-SquidCore.prototype.setCookie = function( cookieName, value, expire )
-{
-  document.cookie = cookieName + '=' + value + '; path=/'
-}
 
 
 // CREDENTIALS / LOGIN
@@ -249,7 +142,7 @@ SquidCore.prototype.setCredentials = function( credentials )
 
   this._credentials = credentials
 
-  this.setCookie( this._CREDENTIALSCOOKIE, this._credentials )
+  window.localStorage[ this._CREDENTIALSCOOKIE ] = credentials
 }
 
 // Return current user `Basic Auth` encoded credentials
@@ -258,6 +151,8 @@ SquidCore.prototype.setCredentials = function( credentials )
 //
 SquidCore.prototype.getCredentials = function()
 {
+  this._credentials = window.localStorage[ this._CREDENTIALSCOOKIE ]
+
   if( !this._credentials )
     throw new Error( 'User credentials are not set' )
 
@@ -271,7 +166,7 @@ SquidCore.prototype.getCredentials = function()
 //
 SquidCore.prototype.isLogin = function()
 {
-  var credentials = this.getCookie( this._CREDENTIALSCOOKIE )
+  var credentials = this.getCredentials()
 
   if( !credentials )
     return false
@@ -280,7 +175,7 @@ SquidCore.prototype.isLogin = function()
 }
 
 // Unset user credentials. 
-// Trigger `user::login` event
+// Trigger `user::logout` event
 //
 //      @return  {void}
 //
@@ -290,9 +185,7 @@ SquidCore.prototype.logout = function()
 
   this._credentials = false
 
-  this.setCookie( this._CREDENTIALSCOOKIE, null )
-
-  this.trigger( 'user::login' )
+  delete window.localStorage[ this._CREDENTIALSCOOKIE ]
 }
 
 
