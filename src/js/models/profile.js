35e1b8a064a4a2653547a39bbc8b09f38d158f41
Michael Lefebvre <michael@scenedata.com>
2015-04-02 00:50:57 +0200

    user profile, draft

--- src/js/models/profile.js
+++ src/js/models/profile.js
@@ -1,16 +1,13 @@
 
-// models.profile = Backbone.Model.extend(
-// {
-//     defaults : 
-//     {
-//         name:         null
-//       , public_repos: null
-//       , avatar_url:   null
-//       , email:        null
-//     }
+var Backbone = require('Backbone')
 
-//   , validate: function ( attrs ) 
-//     {
-//       return ( !/^.+@.+\..+$/.test( attrs.email ) )
-//     }
-// })
+models.profile = Backbone.Model.extend(
+{
+    defaults : 
+    {
+        name:         null
+      , public_repos: null
+      , avatar_url:   null
+      , email:        null
+    }
+})
